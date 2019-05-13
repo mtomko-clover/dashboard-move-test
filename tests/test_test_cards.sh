56d692d82286059e4a7feaeb2ccce4b97d35d8ed
rayramsay <rachel.ramsay@outlook.com>
2019-05-13 20:40:56 +0000

    Fix and lint scripts

--- tests/test_test_cards.sh
+++ tests/test_test_cards.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /home/rachel/virtualenvs/tests/bin/activate
+source /home/rachel/devrel-tools/tests/secrets.sh
 python /home/rachel/devrel-tools/tests/test_test_cards.py
 deactivate
