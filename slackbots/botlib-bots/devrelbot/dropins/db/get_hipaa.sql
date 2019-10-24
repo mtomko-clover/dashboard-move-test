6efe828f8cf76508e12e36cd93503c9d2d8e68b7
tcottingham <tcottingham@Treys-MacBook-Pro.local>
2019-10-24 14:56:08 -0600

    updating get hipaa isvbot command

---
+++ slackbots/botlib-bots/devrelbot/dropins/db/get_hipaa.sql
@@ -0,0 +1,19 @@
+--@enabled true
+
+SELECT DISTINCT(merchant_app.merchant_id), (SELECT NAME
+    FROM   merchant
+    WHERE  merchant.id = merchant_app.merchant_id) AS 'Name'
+FROM   merchant_app
+WHERE  deleted_time IS NULL
+    AND merchant_id IN (SELECT id
+        FROM   merchant
+        WHERE  merchant_plan_id IN (SELECT id
+            FROM   merchant_plan
+            WHERE  app_bundle_id = (SELECT id
+                FROM   app_bundle
+                WHERE  uuid = '7V4586G1JRR60')))
+    AND app_id NOT IN (SELECT developer_app_id
+                        FROM   app_app_bundle
+                        WHERE  app_bundle_id = (SELECT id
+                                                FROM   app_bundle
+                                                WHERE  uuid = '7V4586G1JRR60'));
