b4126f9a09974e4e547949c2ed68472d508b0378
Joel McIntyre <joel.mcintyre@bulldog.corp.clover.com>
2019-10-28 20:33:53 +0000

    update get_hipaa options

--- slackbots/botlib-bots/devrelbot/dropins/db/get_hipaa.sql
+++ slackbots/botlib-bots/devrelbot/dropins/db/get_hipaa.sql
@@ -1,19 +1,18 @@
 --@enabled true
-USE meta;
 SELECT DISTINCT(merchant_app.merchant_id), (SELECT NAME
-    FROM   merchant
+    FROM   meta.merchant
     WHERE  merchant.id = merchant_app.merchant_id) AS 'Name'
-FROM   merchant_app
+FROM   meta.merchant_app
 WHERE  deleted_time IS NULL
     AND merchant_id IN (SELECT id
-        FROM   merchant
+        FROM   meta.merchant
         WHERE  merchant_plan_id IN (SELECT id
-            FROM   merchant_plan
+            FROM   meta.merchant_plan
             WHERE  app_bundle_id = (SELECT id
-                FROM   app_bundle
+                FROM   meta.app_bundle
                 WHERE  uuid = '7V4586G1JRR60')))
     AND app_id NOT IN (SELECT developer_app_id
-                        FROM   app_app_bundle
+                        FROM   meta.app_app_bundle
                         WHERE  app_bundle_id = (SELECT id
-                                                FROM   app_bundle
+                                                FROM   meta.app_bundle
                                                 WHERE  uuid = '7V4586G1JRR60'));
