b836459f5f64f5e7a3fd66ec4fcaf3cc0ccc0381
rachelantion <rachel.antion@clover.com>
2019-12-04 14:11:36 -0700

    got a timer example to work

--- src/components/TimeTracker.tsx
+++ src/components/TimeTracker.tsx
@@ -1,18 +1,43 @@
 import {Component} from "react";
 import * as React from "react";
+import { TestTimer } from "./TestTimer";
 
+interface State {
+    phoneCallDuration: number
+}
 
-export default class TimeTracker extends Component<any, any> {
+export default class TimeTracker extends Component<any, State> {
 
     constructor(props: any) {
         super(props);
+        this.state = {
+            phoneCallDuration: 0
+        }
+    }
+
+    startPhoneCallDuration(): void{
+        const startTime = Date.now() - this.state.phoneCallDuration;
+        const timer = setInterval(() => {
+            this.setState({ phoneCallDuration: Date.now() - startTime });
+        });
+    }
+
+    stopPhoneCallDuration():void {
+
     }
 
     render(): React.ReactNode {
         return (
-           <div>
-               I WILL BE THE TIME TRACKAAAAAA
-           </div>
+            <div>
+                I WILL BE THE TIME TRACKAAAAAA
+                <div>Phone Call
+                    <div>{this.state.phoneCallDuration}</div>
+                <button onClick={this.startPhoneCallDuration}>Start</button></div>
+                <button onClick={this.stopPhoneCallDuration}>Stop</button>
+
+
+                <TestTimer/>
+            </div>
         )
     }
 }
