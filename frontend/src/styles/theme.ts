fff81435da846fc8fd38f34555a4bc158dc619dc
Frank Faustino <nawzyah@gmail.com>
2019-12-19 15:22:15 -0800

    Add /styles/ dir

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,46 @@
+const black = "#262626"
+const white = "#FFFFFF"
+const alabaster = "#FAFAFA"
+const lightGray = "#E8E8E8"
+const mediumGray = "#8C8C8C"
+const darkGray = "#595959"
+const lima = "#52C41A"
+const red = "#F5222D"
+const outerSpace = "#283338"
+const dodgerBlue = "#1890FF"
+const azure = "#108EE9"
+const green = "#00A686"
+
+export const theme = {  
+  // * Misc. -----------------------------------
+  boxShadow: "0 2px 3px 0 rgba(0, 0, 0, 0.10)",
+  line: lightGray,
+  listHeaderBackground: alabaster,
+
+  // * Colors ----------------------------------
+  black,
+  white,
+  lightGray,
+  mediumGray,
+  darkGray,
+
+  highlightLight: lima,
+  highlightDark: red,
+
+  // * Typeface --------------------------------
+  textDark: black,
+  textLight: darkGray,
+  headingDark: outerSpace,
+  headingLight: mediumGray,
+  linkDark: dodgerBlue,
+  linkLight: azure,
+
+  // * Menu ------------------------------------
+  menuBackground: white,
+  menuItemDark: black,
+  menuItemLight: mediumGray,
+
+  // * Header ----------------------------------
+  headerBackground: green,
+  headerText: white
+}
