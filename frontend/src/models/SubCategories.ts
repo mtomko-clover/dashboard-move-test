1d48b5e82240dae8a8f8763f5390e176117ac2a4
rachelantion <rachel.antion@clover.com>
2019-12-19 20:21:40 -0700

    test commit

---
+++ frontend/src/models/SubCategories.ts
@@ -0,0 +1,12 @@
+export enum TAM_APP_APPROVAL {
+    DEVELOPER_SUBMISSION = "Developer Submits Application",
+    PERMISSIONS_REVIEW = "Permissions and Justifications Review",
+    BEGIN_FUNCTIONAL_REVIEW = "Begin Functional Review",
+    FUNCTIONAL_REVIEW_COMPLETE = "Functional Review Complete",
+    DOCUMENTATION_FOR_LEGAL = "Upfront Document Created for Legal",
+    LEGAL_DOCUMENTATION_POSTED = "Upfront Documentation Posted for Legal to Review",
+    PRIVACY_POLICY_REVIEWED = "Privacy Policy is Reviewed",
+    APP_LEGAL_REVIEW = "App Reviewed By Legal for Concept and TOS",
+    APP_REVIEW_COMPLETE = "App and TOS review complete"
+}
+
