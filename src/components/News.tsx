42509b97dc2253531dd76a6d318ac9d50729cda5
rachelantion <rachel.antion@clover.com>
2020-01-02 15:39:19 -0700

    grid is working, but not styled

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,10 +1,17 @@
-import {Component} from "react";
-import * as React from "react";
+import React, {Component, ReactElement} from "react";
 import './TimeTracker.css';
 import styled from "styled-components";
+import NewsUpdate from "../models/NewsUpdate";
+import {NEWS_CATEGORIES} from "../models/NewsCategories";
+import NewsUpdateRow from "./NewsUpdateRow";
+
 const ms = require('pretty-ms');
 
 interface NewsProps {
+}
+
+interface State {
+    updates: NewsUpdate[];
 }
 
 const NewsContainer = styled.div`
@@ -12,6 +19,12 @@ const NewsContainer = styled.div`
     flex-direction: column;
     align-items: center;
     background: white;
+    width: 500px;
+`;
+
+const NewsGrid = styled.div`
+    display: grid;
+    grid-template-columns: 10% 70% 20%;
 `;
 
 const NewsHeader = styled.div`
@@ -24,25 +37,70 @@ const NewsHeader = styled.div`
    padding: 20px;
 `;
 
-const dummyData = {
+const ColumnNames = styled.div`
+    display: grid;
+    grid-template-columns: 10% 70% 20%;
+    width: 100%;
+`;
 
-}
 
-export default class News extends Component<NewsProps, any> {
+const Title = styled.div`
+    grid-column-start: 2;
+`;
+
+
+const Category = styled.div`
+    grid-column-start: 3;
+`;
+
+
+const dummyData =[
+    new NewsUpdate (new Date().getMilliseconds(), "MiCamp multipay tokens issue and GrubHub", NEWS_CATEGORIES.ESCALATION),
+    new NewsUpdate (new Date().getMilliseconds(), "Altria/Smokin Rebates concept - pending", NEWS_CATEGORIES.ISSUE),
+    new NewsUpdate (new Date().getMilliseconds(), "DART: Progress on new DevRel Dashboard design", NEWS_CATEGORIES.DEVREL),
+    new NewsUpdate (new Date().getMilliseconds(), "COE: Organized and announced to the team", NEWS_CATEGORIES.DEVREL),
+    new NewsUpdate (new Date().getMilliseconds(), "ECommerce documentation moving forward", NEWS_CATEGORIES.CONTENT),
+    new NewsUpdate (new Date().getMilliseconds(), "DevsRock: First wave email updates to devs", NEWS_CATEGORIES.DEVREL),
+    new NewsUpdate (new Date().getMilliseconds(), "Developer Advocate responsibilities conflict", NEWS_CATEGORIES.DEVREL),
+    new NewsUpdate (new Date().getMilliseconds(), "Ireland Team Training", NEWS_CATEGORIES.ENGINEERING),
+    new NewsUpdate (new Date().getMilliseconds(), "Sandbox to GCP testing in progress", NEWS_CATEGORIES.DEVREL)
+];
+
+export default class News extends Component<NewsProps, State> {
 
     constructor(props: any) {
         super(props);
+        this.state = {
+          updates: dummyData
+        };
+        this.renderUpdates = this.renderUpdates.bind(this);
+    }
+
+    renderUpdates(): React.ReactElement {
+        const elements: any = [];
+        this.state.updates.forEach( update => {
+            elements.push(<NewsUpdateRow update={update}/>);
+        });
+        let styles = {
+            width: '100%'
+        };
+        return(<div style={styles}>{elements}</div>)
     }
 
     render(): React.ReactNode {
         return (
             <NewsContainer>
                <NewsHeader>
-                   Newsssssss
+                   News
                    <div className="filler"/>
                    <i className="fas fa-ellipsis-h"/>
                </NewsHeader>
-                stuff
+                <ColumnNames>
+                    <div>Date</div>
+                    <Title>Title</Title>
+                    <Category>Type</Category>
+                </ColumnNames>
+                {this.renderUpdates()}
             </NewsContainer>
         )
     }
--- src/models/NewsUpdate.ts
+++ src/models/NewsUpdate.ts
@@ -1,10 +1,14 @@
+import {NEWS_CATEGORIES} from "./NewsCategories";
+
 export default class NewsUpdate {
 
     date: number;
     title: string;
+    category: NEWS_CATEGORIES;
 
-
-    constructor(){
-
+    constructor(date: number, title: string, category: NEWS_CATEGORIES){
+        this.date = date;
+        this.title = title;
+        this.category = category;
     }
 }
