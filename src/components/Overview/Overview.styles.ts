2bb442e0bbae6df403c8ce5d2cb7434ccac51021
Frank Faustino <nawzyah@gmail.com>
2020-01-02 19:02:29 -0800

    Add DatePicker to WeeklyStats

--- src/components/Overview/Overview.styles.ts
+++ src/components/Overview/Overview.styles.ts
@@ -14,3 +14,28 @@ export const Column = styled.div`
     width: 100%;
     max-width: 1400px;
 `;
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px 29px 0 55px;
+    width: 100%;
+    max-width: 1400px;
+
+    h2 {
+        font-size: 18px;
+        font-weight: 900;
+        color: #283338;
+    }
+
+    .ant-input {
+        font-family: Maison Mono !important;
+        font-weight: 400;
+        font-size: 12px;
+    }
+
+    .ant-calendar-picker-icon {
+        margin-top: -10px !important;
+    }
+`;
