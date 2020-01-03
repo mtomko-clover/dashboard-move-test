f75410021f4982cbf1e0c1b3e85dc249fe8cc37b
Frank Faustino <nawzyah@gmail.com>
2020-01-02 17:08:42 -0800

    Add max-width to App and additional BigCard components

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,35 +1,36 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  display: grid;
-  background: ${({ theme }) => theme.headerBackground};
-  padding: 20px;
-  align-items: center;
-  grid-template-columns: 45px 200px 1fr 100px;
+	display: grid;
+	grid-template-columns: 45px 200px 1fr 100px;
+	align-items: center;
+	padding: 20px;
+	width: 100%;
+	background: ${({ theme }) => theme.headerBackground};
 
-  a.header_title {
-    font-size: 20px;
-    font-weight: bold;
-    text-decoration: none;
-    color: white;
-  }
+	a.header_title {
+		font-size: 20px;
+		font-weight: bold;
+		text-decoration: none;
+		color: white;
+	}
 
-  a.time_tracking {
-    text-decoration: none;
-    padding: 10px;
-    font-size: 18px;
-    font-weight: bold;
-    color: white
-  }
+	a.time_tracking {
+		text-decoration: none;
+		padding: 10px;
+		font-size: 18px;
+		font-weight: bold;
+		color: white
+	}
 
-  img {
-    height: 30px;
-    margin-right: 15px;
-    filter: grayscale(1) brightness(1) contrast(10);
-  }
-`
+	img {
+		height: 30px;
+		margin-right: 15px;
+		filter: grayscale(1) brightness(1) contrast(10);
+	}
+`;
 
 export const LogoutButton = styled.button`
-  padding: 10px;
-  border-radius: 5px;
+	padding: 10px;
+	border-radius: 5px;
 `;
