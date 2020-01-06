a3e7e1c1b344d4dda3c00a7220fe3d0784d6e716
RachelAntion <rachel.antion@clover.com>
2020-01-06 14:04:09 -0700

    changes to make franks server work w/ the app (#116)
    * changes to make franks server work w/ the app
    
    * removed comment line

--- frontend/src/utils/graphql.ts
+++ frontend/src/utils/graphql.ts
@@ -1,6 +1,7 @@
 import ApolloClient from "apollo-boost";
 
-const { REACT_APP_GRAPHQL_SERVER } = process.env;
+// const { REACT_APP_GRAPHQL_SERVER } = process.env;
+const  REACT_APP_GRAPHQL_SERVER  = "http://localhost:4000";
 
 const client = new ApolloClient({
   uri: REACT_APP_GRAPHQL_SERVER
