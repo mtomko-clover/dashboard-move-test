abaa124cca4b79cee8163a343b7994066d4260c5
Frank Faustino <nawzyah@gmail.com>
2020-01-06 18:26:10 -0800

    Move WeeklyDigest state to React.Context

--- src/components/Overview/types.ts
+++ src/components/Overview/types.ts
@@ -1,4 +1,6 @@
-import { DocumentNode } from "graphql";
+import {DocumentNode} from "graphql";
+import {Moment} from "moment";
+import {Dispatch, SetStateAction} from "react";
 
 export type Datum = {
   query: DocumentNode;
@@ -22,4 +24,39 @@ export interface DevApprovalsState {
 export interface CommunityState {
 	questions: Datum;
 	answers: Datum;
+}
+
+export type Card = {
+	key: string;
+	title: string;
+	selected: boolean;
+}
+
+export type CardOne = Card & {
+	content: AppApprovalsState;
+}
+
+export type CardTwo = Card & {
+	content: DevApprovalsState;
+}
+
+export type CardThree = Card & {
+	content: CommunityState;
+}
+
+export type CardsState = Array<CardOne | CardTwo | CardThree>;
+
+export type CardsStore = {
+	cards: CardsState;
+	setCards: Dispatch<SetStateAction<CardsState>>;
+}
+
+export type DateStore = {
+	date: Moment;
+	setDate: Dispatch<SetStateAction<Moment>>;
+}
+
+export type Context = {
+	cardsStore: CardsStore;
+	dateStore: DateStore;
 }
