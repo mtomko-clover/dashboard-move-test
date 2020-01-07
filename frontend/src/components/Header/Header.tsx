6c7348bcef8d37027c8515dfd71ba4f3c9e35c9a
rachelantion <rachel.antion@clover.com>
2020-01-06 22:28:23 -0700

    login w/ profile picture

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,37 +1,52 @@
 import React, {ReactElement} from "react";
 import {Link} from "react-router-dom";
-
+import { Menu, Dropdown} from "antd";
 import logo from "../../static/clover-logo.png";
-import {HeaderContainer, LogoutButton} from "./Header.styles";
-
-
+import {HeaderContainer, LogoutButton,  UsernameContainer, ProfilePictureContainer, FullNameContainer} from "./Header.styles";
 type HeaderProps = {
     logout: () => void
     sessionId: string
-    username: React.ReactElement
+    username: string
+    profilePic?: any
 }
-
-
-
-const Header = ({ logout, sessionId, username }: HeaderProps) => (
-    <HeaderContainer>
-        <img alt="Clover Logo" src={logo} />
-        <Link className="header_title" to="/Home">
-            Developer Relations
-        </Link>
-        {sessionId && (
-            <Link className="header_link" to="/TimeTracker">
-                Time Tracking
+const Header = ({ logout, sessionId, username, profilePic}: HeaderProps) => {
+    const menu = (
+        <Menu>
+            <Menu.Item>
+                <div className="row" onClick={logout}>
+                    <i className="fas fa-sign-in-alt font_icon" />
+                    Logout
+                </div>
+            </Menu.Item>
+        </Menu>
+    );
+    return (
+        <HeaderContainer>
+            <img id="logo" alt="Clover Logo" src={logo} />
+            <Link className="header_title" to="/Home">
+                Developer Relations
             </Link>
-        )}
-        {sessionId && (
-            <LogoutButton onClick={logout}>
-                <i className="fas fa-sign-in-alt margin-after" />
-                Logout
-            </LogoutButton>
-        )}
-        {username}
-    </HeaderContainer>
-);
-
+            {sessionId && (
+                <Link className="header_link" to="/TimeTracker">
+                    Time Tracking
+                </Link>
+            )}
+            {sessionId && (
+                <Dropdown overlay={menu}>
+                    <UsernameContainer className="ant-dropdown-link" href="#">
+                        {profilePic && <ProfilePictureContainer src={profilePic} />}
+                        <FullNameContainer>{username}</FullNameContainer>
+                    </UsernameContainer>
+                </Dropdown>
+            )}
+            {!sessionId && (
+                <div></div> //this is a filler so login goes at the end
+            )}
+            {!sessionId && (
+                <Link className="header_link" to="/">
+                    Login
+                </Link>
+            )}
+        </HeaderContainer>
+    )};
 export default Header
--- frontend/src/utils/EmployeeUtil.ts
+++ frontend/src/utils/EmployeeUtil.ts
@@ -1,41 +1,99 @@
 import RachelAntion from "../static/images/employees/Rachel_Antion.jpeg";
 import AdityaSingh from "../static/images/employees/Aditya_Singh.jpeg";
 import BrieRuse from "../static/images/employees/Brie_Ruse.jpeg";
-import Chetan from "../static/images/employees/Chetan_Bhatia.jpeg";
-import Dex from "../static/images/employees/Dex_Cook.jpeg";
-import EmilyLuckek from "../static/images/employees/Emily_Luckek.jpeg";
+import Chetan from "../static/images/employees/Chetan_Bhatia.jpg";
+import Dex from "../static/images/employees/Dex_Cook.jpg";
+import EmilyLucek from "../static/images/employees/Emily_Luckek.png";
 import Frank from "../static/images/employees/Frank_Faustino.jpeg";
-import Joel from "../static/images/employees/Joel_McIntyre.jpeg";
+import Joel from "../static/images/employees/Joel_McIntyre.png";
 import Lauri from "../static/images/employees/Lauri_Cerneck.jpeg";
 import Tomko from "../static/images/employees/Mike_Tomko.jpeg";
-import Nathan from "../static/images/employees/Nathan_Binding.jpeg";
+import Nathan from "../static/images/employees/Nathan_Binding.png";
 import Nazanin from "../static/images/employees/Nazanin_Ahmadkhani.jpeg";
-import NicholasHo from "../static/images/employees/Nicholas_Ho.jpeg";
+import NicholasHo from "../static/images/employees/Nicholas_Ho.jpg";
 import Paul from "../static/images/employees/Paul_Petyo.jpeg";
-import RachelRamsay from "../static/images/employees/Rachel_Ramsay.jpeg";
-import Ram from "../static/images/employees/Ram_Kavasseri.jpeg";
-import Ricardo from "../static/images/employees/Ricardo_Ventura.jpeg";
+import RachelRamsay from "../static/images/employees/Rachel_Ramsay.jpg";
+import Ram from "../static/images/employees/Ram_Kavasseri.png";
+import Ricardo from "../static/images/employees/Ricardo_Ventura.jpg";
+import Richelle from "../static/images/employees/Richelle_Herrli.jpg";
+import Sreya from "../static/images/employees/Sreya_Dutta.jpg";
+import Trey from "../static/images/employees/Trey_Cottingham.jpg";
+import Employee from "../models/Employee";
 
 
 
 
 export class EmployeeUtil {
 
-    public static getEmployeeImage(username: string): string {
-        let image: string = "";
+    public static getEmployeeFromUsername(username: string): Employee {
+        let employee: Employee = new Employee("none");
         console.log("username", username);
         switch (username) {
             case "aditya.singh" :
-                image = AdityaSingh;
+                employee = new Employee("Aditya Singh", AdityaSingh);
                 break;
             case "rachel.antion" :
-                image = RachelAntion;
+                employee = new Employee("Rachel Antion", RachelAntion);
+                break;
+            case "dex.cook" :
+                employee = new Employee("Dex Cook", Dex);
+                break;
+            case "ricardo.ventura":
+                employee = new Employee("Ricardo Ventura", Ricardo);
+                break;
+            case "nathan.binding":
+                employee = new Employee("Nathan Binding", Nathan);
+                break;
+            case "paul.petyo":
+                employee = new Employee("Paul Petyo", Paul);
+                break;
+            case "nicholas.ho":
+                employee = new Employee("Nicholas Ho", NicholasHo);
+                break;
+            case "ram.kavasseri":
+                employee = new Employee("Ram Kavasseri", Ram);
+                break;
+            case "emily.lucek":
+                employee = new Employee("Emily Luckek", EmilyLucek);
+                break;
+            case "lauri.cerneck":
+                employee = new Employee("Lauri Cerneck", Lauri);
+                break;
+            case "mike.tomko":
+                employee = new Employee("Mike Tomko", Tomko);
+                break;
+            case "brie.ruse":
+                employee = new Employee("Brie Ruse", BrieRuse);
+                break;
+            case "nahmadkhani":
+                employee = new Employee("Nazanin Ahmadkhani", Nazanin);
+                break;
+            case "joel.mcintyre":
+                employee = new Employee("Joel McIntyre", Joel);
+                break;
+            case "chetan.bhatian":
+                employee = new Employee("Chetan Bhatian", Chetan);
+                break;
+            case "rachel":
+                employee = new Employee("Rachel Ramsay", RachelRamsay);
+                break;
+            case "frank.faustino":
+                employee = new Employee("Frankizzle Faustino", Frank);
+                break;
+            case "richelle.herrli":
+                employee = new Employee("Richelle Herrli", Richelle);
+                break;
+            case "sreya.dutta":
+                employee = new Employee("Sreya Dutta", Sreya);
+                break;
+            case "tcottingham":
+                employee = new Employee("Trey Cottingham", Trey);
                 break;
             default:
-                image = "none";
+                employee = new Employee(username);
+
         }
-        console.log(image);
-        return image;
+        return employee;
     }
 
 
