05cb5750015f91e2f4dc585f1c50751ee90724f7
Frank Faustino <nawzyah@gmail.com>
2020-01-16 21:20:32 -0800

    ⚡️ Restructure modules

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -1,4 +1,28 @@
+import axios from 'axios'
 import { NextFunction, Request, Response } from 'express'
+import moment from 'moment'
+
+
+export const answerHubAPI = axios.create({
+  baseURL: process.env.ANSWERHUB_URL,
+  headers: {
+    Authorization: `Basic ${process.env.ANSWERHUB_TOKEN}`
+  }
+})
+
+export const jiraAPI = axios.create({
+  baseURL: process.env.JIRA_URL,
+  headers: {
+    Authorization: `Basic ${process.env.JIRA_TOKEN}`
+  }
+})
+
+export function getPriorWeekDates(start: string, end: string, dateFormat: string) {
+  return {
+    startPriorWeek: moment(start).subtract(1, 'week').format(dateFormat),
+    endPriorWeek: moment(end).subtract(1, 'week').format(dateFormat)
+  }
+}
 
 export function formatDate(epoch: number) {
   const date = new Date(epoch * 1000)
---
+++ server/src/modules/news/resolvers.ts
@@ -0,0 +1,36 @@
+import { query } from '../../db/fns'
+
+
+const news = async () => {
+  const response = await query(['SELECT * FROM news;'])
+  console.log('fetchNewsItems: ', response[0])
+  return response[0]
+}
+
+const addNews = async (_: any, args: any) => {
+  try {
+    const { date, title, description, type, username } = args
+    console.log('updateNewsItem: ', date, title, description, type, username)
+
+    const fields = [`"${date}"`, `"${title}"`, `"${description}"`, `"${type}"`, `"${username}"`]  
+    const response = await query([
+      `INSERT INTO news (created_at, title, description, type, author) VALUES (${fields.join(',')});`,
+      'COMMIT;',
+    ])
+    console.log('updateNewsItem response: ', response)
+    // TO-DO: add to user_news table too
+    return { message: 'success!' }
+  } catch ({ message }) {
+    console.error(message)
+    return { message }
+  }
+}
+
+export default {
+  Query: {
+    news
+  },
+  Mutation: {
+    addNews
+  }
+}
