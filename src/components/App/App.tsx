80d045d05a83cb8ed306717434600a17a733399d
Frank Faustino <nawzyah@gmail.com>
2020-01-20 23:07:34 -0800

    Remove Login/Logout from Header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,92 +1,100 @@
-import {notification} from "antd";
+import { notification } from "antd";
 import axios from "axios";
 import Cookie from "js-cookie";
-import React, {ReactElement, useState} from "react";
-import {Route, RouteComponentProps, withRouter} from "react-router-dom";
-import {ThemeProvider} from "styled-components";
+import React, { ReactElement, useState } from "react";
+import { Route, RouteComponentProps, withRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
+import Admin from "../AdminPage";
 import Header from "../Header";
 import Overview from "../Overview";
-import {SignIn} from "../SignIn";
+import { SignIn } from "../SignIn";
 import TimeTracker from "../TimeTracking/TimeTracker";
-import Admin from "../AdminPage";
 
 import * as Constants from "../../utils/Constants";
-import {Environment, Environments} from "../../utils/Environments";
-import {AppContainer} from './App.styles'
-import {GlobalStyles, theme} from '../../styles'
-import {EmployeeUtil} from "../../utils/EmployeeUtil";
+import { Environment, Environments } from "../../utils/Environments";
+import { EmployeeUtil } from "../../utils/EmployeeUtil";
+import { CookiesUtil } from "../../utils/CookiesUtil";
+import { Cookies } from "../../utils/Cookies";
 import Employee from "../../models/Employee";
-import {CookiesUtil} from "../../utils/CookiesUtil";
-import {Cookies} from "../../utils/Cookies";
-import {EMPLOYEE_ROLES} from "../../models/EmployeeRoles";
+import { EMPLOYEE_ROLES } from "../../models/EmployeeRoles";
+import { AppContainer } from "./App.styles";
+import { GlobalStyles, theme } from "../../styles";
 
 interface State {
-    sessionId?: string | undefined;
-    environment?: Environment;
-    user?: Employee;
+	sessionId?: string | undefined;
+	environment?: Environment;
+	user?: Employee;
 }
 
 const App = ({ history }: RouteComponentProps): ReactElement => {
-    const [state, setState] = useState({
-        sessionId: Cookie.get(Constants.sessionIdCookieName),
-        environment: Environments.getDefaultEnvironment(),
-        user: new Employee("",EMPLOYEE_ROLES.NON_ADMIN,"")
-    } as State);
+	const [state, setState] = useState({
+		sessionId: Cookie.get(Constants.sessionIdCookieName),
+		environment: Environments.getDefaultEnvironment(),
+		user: new Employee("", EMPLOYEE_ROLES.NON_ADMIN, ""),
+	} as State);
 
-    const logout = (): void => {
-        Cookie.remove(Constants.sessionIdCookieName);
-        setState({ sessionId: undefined });
-        history.push("/");
-    };
+	const logout = (): void => {
+		Cookie.remove(Constants.sessionIdCookieName);
+		setState({ sessionId: undefined });
+		history.push("/");
+	};
 
-    const useSignIn = async (username: string, password: string, env: string): Promise<string | undefined> => {
-        const environment = Environments.getFromShortTerm(env);
-        setState({ environment });
+	const useSignIn = async (
+		username: string,
+		password: string,
+		env: string,
+	): Promise<string | undefined> => {
+		const environment = Environments.getFromShortTerm(env);
+		setState({ environment });
 
-        const response = await axios({
-            method: "post",
-            url: `/login`,
-            headers: { "Content-Type": "application/json" },
-            data: {
-                username,
-                password,
-                environment
-            },
-        });
-        if (response.data.error) {
-            showError("Login Error", response.data.error);
-        } else {
-            const user = EmployeeUtil.getEmployeeFromUsername(username);
-            CookiesUtil.setCookie(Cookies.USERNAME, username);
-            setState({sessionId: response.data.sessionId, user: user});
-            history.push("/Home");
-        }
+		const response = await axios({
+			method: "post",
+			url: `/login`,
+			headers: { "Content-Type": "application/json" },
+			data: {
+				username,
+				password,
+				environment,
+			},
+		});
 
-        return response.data.sessionId;
-    };
+		if (response.data.error) {
+			showError("Login Error", response.data.error);
+		} else {
+			const user = EmployeeUtil.getEmployeeFromUsername(username);
+			CookiesUtil.setCookie(Cookies.USERNAME, username);
+			setState({ sessionId: response.data.sessionId, user: user });
+			history.push("/home");
+		}
 
-    const showError = (title: string, body: string): void => {
-        notification.open({
-            message: title,
-            description: body,
-            icon: <i className="fas fa-exclamation-triangle"/>,
-        });
+		return response.data.sessionId;
+	};
 
-    };
+	const showError = (title: string, body: string): void => {
+		notification.open({
+			message: title,
+			description: body,
+			icon: <i className="fas fa-exclamation-triangle" />,
+		});
+	};
 
-    return (
-        <ThemeProvider theme={theme}>
-            <AppContainer>
-                <GlobalStyles />
-                <Header logout={logout} sessionId={state.sessionId}/>
-                <Route path="/" exact render={(props): ReactElement => <SignIn {...props} parentHandleSignIn={useSignIn}/>}/>
-                <Route path="/Home" component={Overview}/>
-                <Route path="/Admin" component={Admin}/>
-                <Route path="/TimeTracker" component={TimeTracker} />
-            </AppContainer>
-        </ThemeProvider>
-    );
-}
+	return (
+		<ThemeProvider theme={theme}>
+			<AppContainer>
+				<GlobalStyles />
+				<Header logout={logout} sessionId={state.sessionId} />
+				<Route
+					path="/"
+					exact
+					render={(props): ReactElement => <SignIn {...props} signIn={useSignIn} />}
+				/>
+				<Route path="/home" component={Overview} />
+				<Route path="/admin" component={Admin} />
+				<Route path="/timetracker" component={TimeTracker} />
+			</AppContainer>
+		</ThemeProvider>
+	);
+};
 
 export default withRouter(App);
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,70 +1,62 @@
-import React, {ReactElement} from "react";
-import {Link} from "react-router-dom";
-import {Dropdown, Menu} from "antd";
+import React, { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import { Dropdown, Icon, Menu } from "antd";
 import logo from "../../static/clover-logo.png";
 import {
-    FullNameContainer,
-    HeaderContainer,
-    LogoutButton,
-    ProfilePictureContainer,
-    UsernameContainer
+	FullNameContainer,
+	HeaderContainer,
+	MenuItemRow,
+	ProfilePictureContainer,
+	UsernameContainer,
 } from "./Header.styles";
-import {CookiesUtil} from "../../utils/CookiesUtil";
-import {Cookies} from "../../utils/Cookies";
-import {EmployeeUtil} from "../../utils/EmployeeUtil";
+import { CookiesUtil } from "../../utils/CookiesUtil";
+import { Cookies } from "../../utils/Cookies";
+import { EmployeeUtil } from "../../utils/EmployeeUtil";
 
 type HeaderProps = {
-    logout: () => void;
-    sessionId: string | undefined;
-}
-
+	logout: () => void;
+	sessionId: string | undefined;
+};
 
 const Header = ({ logout, sessionId }: HeaderProps): ReactElement => {
-    const menu = (
-        <Menu>
-            <Menu.Item>
-                <div className="row" onClick={logout}>
-                    <i className="fas fa-sign-in-alt font_icon" />
-                    Logout
-                </div>
-            </Menu.Item>
-            <Menu.Item>
-                <Link to="/Admin">Admin Settings</Link>
-            </Menu.Item>
-        </Menu>
-    );
-    const username = CookiesUtil.getCookie(Cookies.USERNAME);
-    const user = EmployeeUtil.getEmployeeFromUsername(username);
-    const name = user.fullName.length > 0 ? user.fullName : user.username;
+	const menu = (
+		<Menu style={{ width: "150px" }}>
+			<Menu.Item>
+				<MenuItemRow>
+					<Icon type="setting" />
+					<Link to="/admin">Settings</Link>
+				</MenuItemRow>
+			</Menu.Item>
+			<Menu.Item>
+				<MenuItemRow onClick={logout}>
+					<Icon type="logout" />
+					Logout
+				</MenuItemRow>
+			</Menu.Item>
+		</Menu>
+	);
+	const username = CookiesUtil.getCookie(Cookies.USERNAME);
+	const user = EmployeeUtil.getEmployeeFromUsername(username);
+	const name = user.fullName.length > 0 ? user.fullName : user.username;
 
-    return (
-        <HeaderContainer>
-            <img id="logo" alt="Clover Logo" src={logo} />
-            <Link className="header_title" to="/Home">
-                Developer Relations
-            </Link>
-            {/* {sessionId && (
-                <Link className="header_link" to="/TimeTracker">
-                    Time Tracking
-                </Link>
-            )} */}
-            {sessionId && username && (
-                <Dropdown overlay={menu} className="align-end">
-                    <UsernameContainer className="ant-dropdown-link" href="#">
-                        {user.image && <ProfilePictureContainer src={user.image} />}
-                        <FullNameContainer>{name}</FullNameContainer>
-                    </UsernameContainer>
-                </Dropdown>)}
-            {sessionId && !username && <LogoutButton onClick={logout}>Logout</LogoutButton>}
-            {!sessionId && <div className="filler"/>}
-            {/* ^^ to push login to the end*/}
-            {!sessionId && (
-                <Link className="header_link align-text-end" to="/">
-                    Login
-                </Link>
-            )}
-        </HeaderContainer>
-    )
+	return (
+		<HeaderContainer>
+			<img id="logo" alt="Clover Logo" src={logo} />
+			<Link className="header-title" to="/home">
+				Developer Relations
+			</Link>
+			{sessionId && (
+				<Dropdown overlay={menu} className="header-dropdown">
+					<UsernameContainer className="ant-dropdown-link" href="#">
+						{user.image && (
+							<ProfilePictureContainer src={user.image} />
+						)}
+						<FullNameContainer>{name}</FullNameContainer>
+					</UsernameContainer>
+				</Dropdown>
+			)}
+		</HeaderContainer>
+	);
 };
 
 export default Header;
