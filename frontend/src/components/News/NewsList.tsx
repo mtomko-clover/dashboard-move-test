76311026c03525ee1ace192fccde4395fc2ee2fe
rachelantion <rachel.antion@clover.com>
2020-01-21 13:45:37 -0700

    can now view news Item

--- frontend/src/components/News/NewsList.tsx
+++ frontend/src/components/News/NewsList.tsx
@@ -1,50 +1,68 @@
-import { List } from 'antd';
-import { useQuery } from '@apollo/react-hooks';
-import React, { ReactElement, ReactNode } from 'react';
+import {List} from 'antd';
+import {useQuery} from '@apollo/react-hooks';
+import React, {ReactElement, ReactNode, useState} from 'react';
 
 import Row from './Row';
 import NewsItem from '../../models/NewsItem';
-import { news } from '../../utils/queries';
+import {news} from '../../utils/queries';
+import ViewUpdate from "./ViewUpdate";
+import {NEWS_CATEGORIES} from "../../models/NewsCategories";
 
 const NewsList = (): ReactElement | null => {
-  const { data, error, loading } = useQuery(news);
+    const { data, error, loading } = useQuery(news);
+    const initialState = {visible: false, update: new NewsItem("", new Date(), "", NEWS_CATEGORIES.DevRel)};
+    const [ {visible, update}, setModalState] = useState(initialState);
 
-  if (error) {
-    console.error(error);
-    return null;
-  }
 
-  const newsItem = (item: NewsItem | null): void => {
-    console.log('item clicked', item);
-    // TO-DO: add view News item
-    // const username = CookiesUtil.getCookie(Cookies.USERNAME);
+    console.log("NewsList", data);
 
-    // if (state.update && (username === state.update.username)) {
-    //     setState({ showModal: true, update });
-    // } else {
-    //     setState({ showViewNews: true, update });
-    // }
-  };
+    if (error) {
+        console.error(error);
+        return null;
+    }
 
-  return loading ? null : (
-    <List
-      className="full-width margin-bottom"
-      itemLayout="horizontal"
-      dataSource={data.news}
-      pagination={{
-        onChange: (page): void => console.log(page),
-        pageSize: 7,
-        position: 'bottom',
-      }}
-      renderItem={(update: NewsItem): ReactNode =>
-        update && (
-          <List.Item className="horizontal-padding" onClick={(): void => newsItem(update)}>
-            <Row update={update} />
-          </List.Item>
-        )
-      }
-    />
-  );
+    const newsItem = (item: NewsItem | null): void => {
+        console.log('item clicked', item);
+        if (item) {
+            setModalState({visible: true, update: item});
+        }
+        // TO-DO: add view News item
+        // const author = CookiesUtil.getCookie(Cookies.USERNAME);
+
+        // if (state.update && (author === state.update.author)) {
+        //     setState({ showModal: true, update });
+        // } else {
+        //     setState({ showViewNews: true, update });
+        // }
+    };
+
+    const closeViewUpdate = (): void => {
+        setModalState({visible: false, update: update});
+    };
+
+
+    return loading ? null : (
+        <div className="flex-grow column">
+            {visible && <ViewUpdate showModal={visible} modalClosed={closeViewUpdate} update={update}/>}
+            <List
+                className="full-width margin-bottom"
+                itemLayout="horizontal"
+                dataSource={data.news}
+                pagination={{
+                    onChange: (page): void => console.log(page),
+                    pageSize: 7,
+                    position: 'bottom',
+                }}
+                renderItem={(update: NewsItem): ReactNode =>
+                    update && (
+                        <List.Item className="horizontal-padding" onClick={(): void => newsItem(update)}>
+                            <Row update={update} />
+                        </List.Item>
+                    )
+                }
+            />
+        </div>
+    );
 };
 
 export default NewsList;
